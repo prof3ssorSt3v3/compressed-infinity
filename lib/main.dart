e71edc3090db02f89712f0ee8e9753376e2f3ef8
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2025-11-10 21:05:56 -0500

    add base theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:class2/themes/styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, theme: ThemeData(), home: MyStack());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: MyStack(),
+    );
   }
 }
 
---
+++ lib/themes/styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+  textTheme: TextTheme(
+    displayMedium: TextStyle(fontSize: 60),
+    headlineMedium: TextStyle(fontSize: 42),
+    titleMedium: TextStyle(fontSize: 36),
+    bodyMedium: TextStyle(fontSize: 24),
+    labelMedium: TextStyle(fontSize: 18),
+  ),
+);
